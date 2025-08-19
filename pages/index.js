2acb6aa28efbf616a48ce626233ca7da3351826f
Bryan7905 <galangbryan99@gmail.com>
2025-08-19 13:44:28 +0800

    updated description

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello I am Bryan Oliver M. Galang. I am a 3rd year IT student.</p>
+        <p>Hello I am Bryan Oliver M. Galang. I am a 3rd year IT student. My hobbies are playing e-games, watching movies, and eating freely. I have knowledge in coding.</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
